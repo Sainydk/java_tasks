6ddbde1cfa66756dac9d76af2df8e8ca475a9c3f
Роман Новиков <chVeZcv[jALy\n:(>
2024-01-05 15:30:36 +0300

    Задача 1.1.1

---
+++ src/main/java/JavaApplication8.java
@@ -0,0 +1,5 @@
+public class JavaApplication8 {
+    public static void main(String[] args) {
+        System.out.println("Поздравляю Председателя Свободной КНДР с успешным запуском в епонском море!");
+    }
+}
