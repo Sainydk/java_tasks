ee195a999bfdcddfe02cc4d3a04f3965df7421c8
Роман Новиков <chVeZcv[jALy\n:(>
2024-01-05 15:33:21 +0300

    Задача 1.1.2-а

--- src/main/java/JavaApplication8.java
+++ src/main/java/JavaApplication8.java
@@ -1,5 +1,18 @@
 public class JavaApplication8 {
     public static void main(String[] args) {
         System.out.println("Поздравляю Председателя Свободной КНДР с успешным запуском в епонском море!");
+
+        int x = 5;
+        int y = 7;
+        int z = 0;
+
+        z = x + y; // помещаем в z значение суммы 5+7=12
+        System.out.println(z);
+        z = x - y; // помещаем в z значение разности 5-7=-2
+        System.out.println(z);
+        z = x * y; // помещаем в z значение произведения 5*7=35
+        System.out.println(z);
+        z = x / y; // помещаем в z значение произведения 5/7=0, так как тип int
+        System.out.println(z);
     }
 }
