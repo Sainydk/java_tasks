baa08ea5c0c2b39b0bbca9c9f5084327a9d14122
Роман Новиков <chVeZcv[jALy\n:(>
2024-01-05 16:56:46 +0300

    Задача 1.1.7-c

--- src/main/java/javaapplication8/Calculator.java
+++ src/main/java/javaapplication8/Calculator.java
@@ -1,14 +1,17 @@
-/*Задача 1.1.7-b (Тема 4).
-Пусть x = 5, y = 10;
-Выведите на экран sqrt (x*x + y*y);
-Это и есть гипотенуза треугольника со сторонами x, y.*/
+/*Задача 1.1.7-c (Тема 4). Проведите вычисления
+
+x = 10, y = 20;
+
+z = sqrt (y-x);
+
+z = z +1;*/
 
 package javaapplication8;
 
 public class Calculator {
     public static void main(String[] args) {
-        int x = 5;
-        int y = 10;
-        System.out.println(Math.sqrt((x * x) + (y * y)));
+        int x = 10;
+        int y = 20;
+        System.out.println(Math.sqrt(y - x) + 1);
     }
 }
