95cf70dfc385d59b3cf122edeb534fe76f88e681
Роман Новиков <chVeZcv[jALy\n:(>
2024-01-05 16:51:46 +0300

    Задача 1.1.7-a

--- src/main/java/javaapplication8/Calculator.java
+++ src/main/java/javaapplication8/Calculator.java
@@ -1,19 +1,11 @@
-/*Задача 1.1.6 (Тема 4). Объявите 2 переменных типа double со значениями
-x = 0.25, y = 0.9;
-Посчитайте выражения, каждый раз выводя их на экране:
-10*x + y
-y * sqrt (x)
-возведите x в степень y и затем вычтите 1*/
+/*Задача 1.1.7-a (Тема 4). Пусть x = -10; сколько будет Math.sqrt (x) ?
+Выведите на печать это значение.*/
 
 package javaapplication8;
 
 public class Calculator {
     public static void main(String[] args) {
-        double a = 0.25;
-        double b = 0.9;
-
-        System.out.println(10 * a + b);
-        System.out.println(b * Math.sqrt(a));
-        System.out.println(Math.pow(a, b) - 1);
+        int x = 1;
+        System.out.println(Math.sqrt(x));
     }
 }
